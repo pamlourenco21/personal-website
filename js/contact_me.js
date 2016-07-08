f4028e11039b818e838945a495bea82888efaa34
pamlourenco21 <patriciaraquel.lourenco@live.com>
2016-07-08 16:45:27 +0100

    sections complete

--- js/contact_me.js
+++ js/contact_me.js
@@ -33,7 +33,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>A sua mensagem foi enviada. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -45,16 +45,17 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>" + firstName + ", desculpe mas parece que o servidor não está a responder. " +
+                        "Tente novamente mais tarde!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
+                    //$('#contactForm').trigger("reset");
+                }
             })
         },
         filter: function() {
             return $(this).is(":visible");
-        },
+        }
     });
 
     $("a[data-toggle=\"tab\"]").click(function(e) {
