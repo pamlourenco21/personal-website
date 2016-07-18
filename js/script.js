497b1bd560a0866acd8c58274d85edce094a6cb7
pamlourenco21 <patriciaraquel.lourenco@live.com>
2016-07-18 14:13:22 +0100

    little changes

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
+
+
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
@@ -15,23 +17,28 @@ $(function() {
     });
 });
 
+
+
+
+
+
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
     target: '.navbar-fixed-top'
 });
+
+
+
+
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
-// if (navigator.userAgent.match(/(iPod|iPhone|iPad|Android)/)) {
-//     var $el = $('[data-parallax="scroll"].parallax-fix');
-//     $el.each(function(pos) {
-//         var $newEl = $el[pos];
-//         $newEl.innerHTML = '<div class="parallax-overlay-fix">' + $newEl.innerHTML + '</div>';
-//     });
-// }
+
+
+
 
 
 
